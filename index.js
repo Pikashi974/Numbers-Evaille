300410d2788cf1f102d041f0e7d954ecd141a8be
Jérémy PAYET <the_big_boss97437@hotmail.fr>
2023-12-12 23:58:27 +0400

    Corrected the href for About

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/index.html")); //__dirname : It will resolve to your project folder.
 });
-router.get("/about", function (req, res) {
+router.get("/src/html/about", function (req, res) {
   res.sendFile(path.join(__dirname + "/src/html/about.html"));
 });
 router.get("/sitemap", function (req, res) {
